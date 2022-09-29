c65527011e1c6b2f6f12ffc47f9f97cbea11bb88
Yongun Seong <nevivurn@nevi.dev>
2022-09-30 00:11:07 +0900

    feat(ci): use nix for builds (#1)
    * wip nix
    
    * use stdenvNoCC
    
    * refactor flake and update deps
    
    * feat(ci): test using nix in actions
    
    * empty to trigger ci
    
    * feat(ci): add publish step

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ hugo nodejs node2nix ];
+        };
+      packages.${system}.nevi-dev =
+        let
+          nodeDependencies = (import ./node.nix { inherit pkgs; }).nodeDependencies;
+        in
+        pkgs.stdenvNoCC.mkDerivation {
+          name = "nevi-dev";
+          src = builtins.path { path = ./.; name = "nevi-dev"; };
+          nativeBuildInputs = with pkgs; [ nodejs ];
+
+          postPatch = ''
+            ln -sf ${nodeDependencies}/lib/node_modules node_modules
+          '';
+
+          buildPhase = ''
+            runHook preBuild
+            ${pkgs.hugo}/bin/hugo --minify -d $out/public
+            runHook postBuild
+          '';
+
+          installPhase = "true";
+        };
+      defaultPackage.x86_64-linux = self.packages.${system}.nevi-dev;
+  };
+}
