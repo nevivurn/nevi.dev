668e102ceb4caca46863f45407adfbde51a7f937
Yongun Seong <nevivurn@nevi.dev>
2024-04-14 01:22:44 +0900

    chore(deps): update npm hash

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           npmDeps = pkgs.fetchNpmDeps {
             name = "nevi-dev-npm-deps";
             src = ./.;
-            hash = "sha256-89KJXzyUktLQJzhov/hWCsKKDWdY6dZTuC51pWiRe7Q=";
+            hash = "sha256-h006P7THkld8WUtYisdB4d4qVGfmzYhNcVZ9gWVhpYg=";
           };
         in pkgs.stdenvNoCC.mkDerivation {
           name = "nevi-dev";
