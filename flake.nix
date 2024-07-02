838916b33c1919eefebdb6fb67f67b9598737f41
Yongun Seong <nevivurn@nevi.dev>
2024-07-03 05:30:59 +0900

    refactor(flake): fmt

--- flake.nix
+++ flake.nix
@@ -7,14 +7,25 @@
     resume.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, resume }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      resume,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        runtimeDeps = with pkgs; [ hugo nodejs ];
+        runtimeDeps = with pkgs; [
+          hugo
+          nodejs
+        ];
         resumePDF = "${resume.packages.${system}.default}/resume.pdf";
         resumeVersion = resume.shortRev;
-      in {
+      in
+      {
         apps.serve = flake-utils.lib.mkApp {
           drv = pkgs.writeShellApplication {
             name = "hugo-serve";
@@ -27,36 +38,38 @@
             '';
           };
         };
-        packages.default = let
-          npmDeps = pkgs.fetchNpmDeps {
-            name = "nevi-dev-npm-deps";
+        packages.default =
+          let
+            npmDeps = pkgs.fetchNpmDeps {
+              name = "nevi-dev-npm-deps";
+              src = ./.;
+              hash = "sha256-RD+iyBjBcI9bIafZIbfXSLVLv9qLrugoOQ08UWA7GM4=";
+            };
+          in
+          pkgs.stdenvNoCC.mkDerivation {
+            name = "nevi-dev";
             src = ./.;
-            hash = "sha256-RD+iyBjBcI9bIafZIbfXSLVLv9qLrugoOQ08UWA7GM4=";
+
+            inherit npmDeps;
+            nativeBuildInputs = runtimeDeps ++ (with pkgs; [ npmHooks.npmConfigHook ]);
+
+            preBuild =
+              ''
+                hugo gen chromastyles --style dracula > assets/highlight-dracula.css
+                cp ${resumePDF} 'static/Yongun_Seong_resume-${resumeVersion}.pdf'
+                sed -i 's/resume\.pdf/Yongun_Seong_resume-${resumeVersion}\.pdf/' config.toml
+              ''
+              + nixpkgs.lib.optionalString (self ? shortRev) ''
+                sed -i 's/DRAFT/${self.shortRev}/' config.toml
+              '';
+
+            buildPhase = ''
+              runHook preBuild
+              hugo --minify -d $out/public
+              runHook postBuild
+            '';
+            dontFixup = true; # don't strip CTF challenge binaries etc.
           };
-        in pkgs.stdenvNoCC.mkDerivation {
-          name = "nevi-dev";
-          src = ./.;
-
-          inherit npmDeps;
-          passthru = { inherit npmDeps; };
-
-          nativeBuildInputs = runtimeDeps
-            ++ (with pkgs; [ npmHooks.npmConfigHook ]);
-
-          preBuild = ''
-            hugo gen chromastyles --style dracula > assets/highlight-dracula.css
-            cp ${resumePDF} 'static/Yongun_Seong_resume-${resumeVersion}.pdf'
-            sed -i 's/resume\.pdf/Yongun_Seong_resume-${resumeVersion}\.pdf/' config.toml
-          '' + nixpkgs.lib.optionalString (self ? shortRev) ''
-            sed -i 's/DRAFT/${self.shortRev}/' config.toml
-          '';
-
-          buildPhase = ''
-            runHook preBuild
-            hugo --minify -d $out/public
-            runHook postBuild
-          '';
-          dontFixup = true; # don't strip CTF challenge binaries etc.
-        };
-      });
+      }
+    );
 }
